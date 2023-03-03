d562d555990a724e45859931ce704e7d9ee96f7d
BarirNada00 <116973510+BarirNada00@users.noreply.github.com>
2023-03-03 12:04:23 +0100

    Add files via upload

--- ExampleInstrumentedTest.java
+++ ExampleInstrumentedTest.java
@@ -1,67 +1,80 @@
-package ma.projet.android.uiautomatortests;
+package ma.projet.android.uiautomator3;
 
 import android.content.Context;
-import android.os.RemoteException;
-import android.support.test.uiautomator.UiDevice;
+import android.content.Intent;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
-import org.junit.After;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObject2;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
-
-    @After
-    public void wrapUp() {
-        System.out.println("Test done");
-    }
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "ma.projet.android.uiautomator3";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private UiDevice device;
 
     @Before
-    public void setComponent() {
-        System.out.println("Components initialize");
+    public void startMainActivityFromHomeScreen() {
+
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        device.pressHome();
+
+        final String launcherPackage = device.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
     }
 
     @Test
-    public void testBackKeyPress() {
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).pressBack();
-    }
+    public void test() throws UiObjectNotFoundException {
+        UiObject number1 = device.findObject(new UiSelector()
+                .resourceId("ma.projet.android.uiautomator:id/number1")
+                .className("android.widget.EditText"));
+        number1.setText(5 + "");
+        UiObject number2 = device.findObject(new UiSelector()
+                .resourceId("ma.projet.android.uiautomator:id/number2")
+                .className("android.widget.EditText"));
+        number2.setText(7 + "");
+        UiObject plus = device.findObject(new UiSelector()
+                .text("+")
+                .className("android.widget.Button"));
 
-    @Test
-    public void testUi() throws RemoteException {
-        UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        if (uiDevice.isScreenOn()) {
-            uiDevice.setOrientationLeft();
-            uiDevice.openNotification();
-            uiDevice.openQuickSettings();
-            uiDevice.pressHome();
+        if (plus.exists() && plus.isEnabled()) {
+            plus.click();
         }
-    }
 
-    @Test
-    public void testComplexUi() throws RemoteException {
-        UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        if (uiDevice.isScreenOn()) {
-            uiDevice.openNotification();
-            uiDevice.pressBack();
-            uiDevice.openQuickSettings();
-            uiDevice.freezeRotation();
-            uiDevice.setOrientationLeft();
-            uiDevice.setOrientationRight();
-            uiDevice.pressBack();
-            uiDevice.pressHome();
-        }
+        UiObject2 result = device.findObject(By.res(BASIC_SAMPLE_PACKAGE, "result"));
+        assertEquals("12", result.getText());
     }
 }
